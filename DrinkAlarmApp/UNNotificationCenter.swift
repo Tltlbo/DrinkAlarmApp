bd2046d0eb2c64a02a799292f03bff084fd5a210
TLtlbo <js1436kt@gmail.com>
2023-08-05 22:13:19 +0900

    NotificationCenter와 UserNotifications 라이브러리를 이용한 알림 허용 기능 및 사운드, 뱃지 기능 구현

---
+++ DrinkAlarmApp/UNNotificationCenter.swift
@@ -0,0 +1,19 @@
+//
+//  UNNotificationCenter.swift
+//  DrinkAlarmApp
+//
+//  Created by 박진성 on 2023/08/05.
+//
+
+import Foundation
+import UserNotifications
+
+extension UNUserNotificationCenter {
+    func addNotificationRequest(by alert : Alert) {
+        let content = UNMutableNotificationContent()
+        content.title = "물 마실 시간이에요"
+        content.body = "세계보건기구(WHO)가 권장하는 하루 물 섭취량은 1.5 ~ 2L 입니다"
+        content.sound = .default
+        content.badge = 1
+    }
+}
